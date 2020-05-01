99ceabaedfaccaedcac8e64af967021984473a52
wojnillowicz <64035391+wojnillowicz@users.noreply.github.com>
2020-05-01 18:02:14 +0200

    Update settings.py

--- project_four/project_four/settings.py
+++ project_four/project_four/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qo!l(*rkqodogf5+*0!faw3352=l-!vh$aijilw+6@6hk=e%ba'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
